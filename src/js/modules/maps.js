1be48dc2ff757e53f2394ff596dbe4fa6b2a99de
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-08-13 14:12:29 -0300

    Fix: resolvido bug em que initMap era executado em todas as páginas

--- src/js/modules/maps.js
+++ src/js/modules/maps.js
@@ -1,16 +1,18 @@
 export default function initMap(leafletObject) {
   const maps = document.querySelectorAll(".map");
-  maps.forEach((item) => {
-    const L = leafletObject;
-    const coordinatesString = item.dataset.coordinates.split(",");
-    const coordinates = coordinatesString.map((cord) => Number(cord.trim()));
-    console.log(coordinates);
+  if (maps) {
+    maps.forEach((item) => {
+      const L = leafletObject;
+      const coordinatesString = item.dataset.coordinates.split(",");
+      const coordinates = coordinatesString.map((cord) => Number(cord.trim()));
+      console.log(coordinates);
 
-    var map = L.map(`${item.id}`).setView(coordinates, 16);
+      var map = L.map(`${item.id}`).setView(coordinates, 16);
 
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      maxZoom: 19,
-      attribution: "© OpenStreetMap",
-    }).addTo(map);
-  });
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution: "© OpenStreetMap",
+      }).addTo(map);
+    });
+  }
 }
